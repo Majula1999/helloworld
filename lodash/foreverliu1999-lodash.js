0a2f6c1af868a8e26f5175245d55aa69c332facd
Ashenone <2657518095@qq.com>
2022-01-10 16:17:54 +0800

    TEST

--- lodash/foreverliu1999-lodash.js
+++ lodash/foreverliu1999-lodash.js
@@ -1,5 +1,15 @@
 var foreverliu1999 = {
-  chunk: function () {}
+  chunk: (ary, index) => {
+    let newAry1 = []
+    let newAry2 = []
+    for (let i = 0; i < index; i++) {
+      newAry1.push(ary[i])
+    }
+    for (let i = index; i < ary.length; i++) {
+      newAry2.push(ary[i])
+    }
+    return [newAry1, newAry2]
+  }
 }
 // test1
 // test2
