9b1258dd91750d6e32878058a21128174b4eb23e
Ashenone <2657518095@qq.com>
2022-01-11 20:45:37 +0800

    快速排序

--- everyday/day43-day~ javascript/Day72/demo.js
+++ everyday/day43-day~ javascript/Day72/demo.js
@@ -1,1 +1,19 @@
-// test
+// 把函数f的前若干个参数固定为fixArgs
+function bind (f, ...fixedArgs) {
+
+  return function (...args) {
+    return f(...fixedArgs, ...args);
+  }
+}
+
+// 快速排序
+function quickSort (ary) {
+  let randomIdx = Math.floor (Math.random() * ary.length);
+  if (ary.length <= 1) {
+    return arr;
+  }
+  let standPoint = Math.floor(arr.length / 2);
+  var sP = splice(standPoint, 1)
+  let left = [];
+  let right = [];
+}
