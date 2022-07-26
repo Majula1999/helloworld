89b22070fc0cda17b766dfd0888f7987dd10462e
Ashenone <2657518095@qq.com>
2022-07-26 09:58:45 +0800

    test

---
+++ everyday/day43-day~ javascript/Day82/mySet.js
@@ -0,0 +1,19 @@
+function MySet() {
+
+}
+MySet.prototype.add = function () {}
+MySet.prototype.delete = function () {}
+MySet.prototype.has = function () {}
+MySet.prototype.clear = function () {}
+MySet.prototype.add = function () {}
+MySet.prototype.add = function () {}
+
+function MyMap() {
+MyMap.prototype.set = function () {}
+MyMap.prototype.get = function () {}
+MyMap.prototype.has = function () {}
+MyMap.prototype.clear = function () {}
+MyMap.prototype.delete = function () {}
+
+}
+
